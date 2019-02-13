0418e3619709e6315e5802799c88513b53177ef9
kenzooooo <ruby.on.nishizawa@gmail.com>
2019-02-13 16:30:30 +0900

    [modify] database

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,14 +7,15 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server 'jii', user: 'jii', roles: %w{app assets batch db}
+#server 'jii', user: 'jii', roles: %w{app assets batch db}
+server 'jii', user: 'jii', roles: %w{app batch db}
 
 set :assets_roles, [:assets]
 
 # rails
 set :rails_env, 'production'
 set :branch, 'master'
-set :deploy_to, '/home/factory'
+set :deploy_to, '/home/jii/factory'
 set :log_level, :debug
 set :default_shell, '/bin/bash -l'
 
