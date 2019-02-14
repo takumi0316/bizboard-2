da80c6905d04b2f4adf5531ea1ade2bdd3503847
kenzooooo <ruby.on.nishizawa@gmail.com>
2019-02-14 16:45:38 +0900

    [modify]

--- db/migrate/20190213105028_add_mf_id_to_companies.rb
+++ db/migrate/20190213105028_add_mf_id_to_companies.rb
@@ -1,7 +1,7 @@
 class AddMfIdToCompanies < ActiveRecord::Migration[5.2]
   def change
 
-    add_column :companies, :mf_company_id, :integer
-    add_column :company_divisions, :mf_company_division_id, :integer
+    add_column :companies, :mf_company_id, :string
+    add_column :company_divisions, :mf_company_division_id, :string
   end
 end
