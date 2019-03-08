cc2a2e0bba7b87496f6173c9388994fbddfb6a4b
t.okada <t.okada@jii-h-cpc01.jiinet.local>
2019-03-08 17:29:44 +0900

    [fix]history

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -10,6 +10,13 @@ class HistoriesController < ApplicationController
   #  ** Instance variables **
   #----------------------------------------
 
+  # 活動履歴
+  expose(:histories) {History.all}
+
+  # 取引先
+  expose(:history) { History.find_or_initialize_by id: params[:id] || params[:history_id] }
+
+
   #----------------------------------------
   #  ** Layouts **
   #----------------------------------------
@@ -23,15 +30,89 @@ class HistoriesController < ApplicationController
   #----------------------------------------
 
   ##
+  # 一覧
+  # @version 2018/06/10
+  #
+  def index
+
+    add_breadcrumb '活動履歴'
+  end
+
+  ##
   # 新規作成
-  # @version 2019/03/06
+  # @version 2018/06/10
   #
   def new
 
+    add_breadcrumb '活動履歴', path: histories_path
+    add_breadcrumb '新規作成'
+  end
+
+  ##
+  # 編集
+  # @version 2018/06/10
+  #
+  def edit
+
+    add_breadcrumb '活動履歴', path: histories_path
+    add_breadcrumb '編集'
+  rescue => e
+    redirect_back fallback_location: url_for({action: :index}), flash: {notice: {message: e.message}}
+  end
+
+  ##
+  # 更新処理
+  # @version 2018/06/10
+  #
+  def update
+
+    # 取引先情報更新
+    history.update! history_params
+
+    redirect_back fallback_location: url_for({action: :index}), flash: {notice: {message: '取引先情報を更新しました'}}
+  rescue => e
+
+    redirect_back fallback_location: url_for({action: :index}), flash: {notice: {message: e.message}}
+  end
+
+  ##
+  # 新規作成
+  # @version 2018/06/10
+  #
+  def create
+
+    # 取引先情報更新
+    history.update! history_params
+
+    redirect_to edit_history_path(history), flash: {notice: {message: '取引先情報を更新しました'}}
+  rescue => e
+
+    redirect_back fallback_location: url_for({action: :index}), flash: {notice: {message: e.message}}
+  end
+
+  ##
+  # 削除
+  # @version 2018/06/10
+  #
+  def destroy
+
+    history.destroy
+  rescue => e
+
+    flash[:warning] = { message: e.message }
+  ensure
+    redirect_to action: :index
   end
 
   #----------------------------------------
   #  ** Methods **
   #----------------------------------------
+
+  private
+
+  def history_params
+
+    params.require(:history).permit(:date, :content, :user, :memo, :attachment)
+  end
 
 end
