04b0a5e0a43953ad8c5be7ed8f670310684f84a3
大島勇人　 <oshimayuto@jii-h-cpc66.jiinet.local>
2019-04-10 15:35:14 +0900

    fix[WorkSubcontractor-status]

--- app/javascript/components/work_subcontractor/read-subcontractor_status/index.jsx
+++ app/javascript/components/work_subcontractor/read-subcontractor_status/index.jsx
@@ -30,18 +30,19 @@ export default class ReadSubcontractorStatus extends React.Component {
   render() {
     return(
       <div className={ Style.WorkStatus }>
-        { this.state.subcontractor_status === 'draft' ? 
+        { console.log(this.state.subcontractor_status) }
+        { this.state.subcontractor_status == 0 ? 
           <button className={ 'c-btnMain-primaryC' }>見作業</button>
           : 
           <button className={ 'c-btnMain-negative' }>見発注</button>
 
         }
-        { this.state.subcontractor_status === 'deliverd' ?
+        { this.state.subcontractor_status == 10 ?
           <button className={ 'c-btnMain-primaryC' }>発注済み</button> 
           : 
           <button className={ 'c-btnMain-negative' }>発注済み</button> 
         }
-        { this.state.subcontractor_status === 'complete' ? 
+        { this.state.subcontractor_status == 20 ? 
         <button className={ 'c-btnMain-primaryC' }>納品済み</button>
         :
         <button className={ 'c-btnMain-negative' }>納品済み</button>
