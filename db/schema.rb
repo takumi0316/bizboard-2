ea1949237dd635be7db474785b192c4511ce1403
kenzooooo <ruby.on.nishizawa@gmail.com>
2019-05-17 16:12:03 +0900

    a

--- db/schema.rb
+++ db/schema.rb
@@ -263,9 +263,9 @@ ActiveRecord::Schema.define(version: 2019_05_10_085931) do
     t.integer "binding_work", limit: 1, default: 0
     t.integer "after_process", limit: 1, default: 0
     t.text "note"
-    t.integer "status", limit: 1, default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", limit: 1, default: 0
     t.text "free_word"
     t.bigint "project_number"
     t.integer "price", default: 0
@@ -371,7 +371,6 @@ ActiveRecord::Schema.define(version: 2019_05_10_085931) do
     t.text "note"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "free_word"
     t.index ["subcontractor_id"], name: "index_subcontractor_divisions_on_subcontractor_id"
   end
 
