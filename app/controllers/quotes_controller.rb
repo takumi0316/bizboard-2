c81e88cf6c0b74bd1827d209f530f59807631cb6
YutoOshima <oshima1998@gmail.com>
2019-05-28 14:03:18 +0900

    fix

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -104,6 +104,7 @@ class QuotesController < ApplicationController
         params[:specifications].each do |specification|
 
           parse_json = JSON.parse(specification)
+          binding.pry
           Quote.last.quote_projects.create!(name: parse_json['projectSpecificationName'], unit_price: parse_json['projectSpecficationUnitPrice'].to_i, unit: parse_json['projectSpecificationUnit'].to_i, price: parse_json['projectSpecificationPrice'].to_i)
         end
         render json: { status: :success, quote: Quote.last, quote_projects: Quote.last.quote_projects }
