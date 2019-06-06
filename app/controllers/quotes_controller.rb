a9e2dcf6d85cfe34f9aea34bbad048df97ecdc0a
YutoOshima <oshima1998@gmail.com>
2019-06-06 11:12:03 +0900

    fix

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -107,7 +107,7 @@ class QuotesController < ApplicationController
 
           parse_json = JSON.parse(specification)
           createQuote = Quote.last
-          createQuote.quote_projects.create!(name: parse_json['projectSpecificationName'], unit_price: parse_json['projectSpecficationUnitPrice'].to_i, unit: parse_json['projectSpecificationUnit'].to_i, price: parse_json['projectSpecificationPrice'].to_i, project_id: parse_json['projectSpecificationId'], project_name: parse_json['projectName'], project_id: parse_json['projectId'])
+          createQuote.quote_projects.create!(name: parse_json['projectSpecificationName'], detail: parse_json['projectSpecificationDetail'], unit_price: parse_json['projectSpecficationUnitPrice'].to_i, unit: parse_json['projectSpecificationUnit'].to_i, price: parse_json['projectSpecificationPrice'].to_i, project_id: parse_json['projectSpecificationId'], project_name: parse_json['projectName'], project_id: parse_json['projectId'])
         end
       end
       render json: { status: :success, quote: Quote.last, quote_projects: Quote.last.quote_projects }
@@ -122,10 +122,10 @@ class QuotesController < ApplicationController
           findQuoteProject = QuoteProject.find_or_initialize_by(id: parse_json['projectSpecificationId'])
           if findQuoteProject.id == 0
 
-            QuoteProject.create!(name: parse_json['projectSpecificationName'], unit_price: parse_json['projectSpecificationUnitPrice'].to_i, unit: parse_json['projectSpecificationUnit'].to_i, price: parse_json['projectSpecificationPrice'], quote_id: params[:id], project_id: parse_json['projectSpecificationId'], project_name: parse_json['projectName'], project_id: parse_json['projectId'])
+            QuoteProject.create!(name: parse_json['projectSpecificationName'], detail: parse_json['projectSpecificationDetail'], unit_price: parse_json['projectSpecificationUnitPrice'].to_i, unit: parse_json['projectSpecificationUnit'].to_i, price: parse_json['projectSpecificationPrice'], quote_id: params[:id], project_id: parse_json['projectSpecificationId'], project_name: parse_json['projectName'], project_id: parse_json['projectId'])
           else
 
-            findQuoteProject.update!(name: parse_json['projectSpecificationName'], unit_price: parse_json['projectSpecificationUnitPrice'], unit: parse_json['projectSpecificationUnit'], price: parse_json['projectSpecificationPrice'], project_id: parse_json['projectSpecificationId'], project_name: parse_json['projectName'], project_id: parse_json['projectId'])
+            findQuoteProject.update!(name: parse_json['projectSpecificationName'], detail: parse_json['projectSpecificationDetail'], unit_price: parse_json['projectSpecificationUnitPrice'], unit: parse_json['projectSpecificationUnit'], price: parse_json['projectSpecificationPrice'], project_id: parse_json['projectSpecificationId'], project_name: parse_json['projectName'], project_id: parse_json['projectId'])
           end
         end
       end
