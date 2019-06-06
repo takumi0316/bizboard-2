5cc4acbb0f948c976904eb1642b4c573ef6df574
YutoOshima <oshima1998@gmail.com>
2019-06-06 16:57:32 +0900

    fix[migration]

--- db/migrate/20190606071126_change_column_to_quote_projects.rb
+++ db/migrate/20190606071126_change_column_to_quote_projects.rb
@@ -1,11 +1,11 @@
 class ChangeColumnToQuoteProjects < ActiveRecord::Migration[5.2]
   def up
 
-  	change_column :quote_projects, :unit_price, :string
+  	change_column :quote_projects, :unit_price, :decimal, precision: 10, scale: 3
   end
 
   def down
 
-    change_column :quote_projects, :unit_price, :decimal, precision: 10, scale: 3
+  	change_column :quote_projects, :unit_price, :string
   end
 end
