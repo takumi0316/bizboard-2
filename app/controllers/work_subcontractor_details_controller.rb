46124e75593815dc313144d4ba025d9feaae7bc1
YutoOshima <oshima1998@gmail.com>
2019-06-26 11:47:41 +0900

    fix[色々修正]

--- app/controllers/work_subcontractor_details_controller.rb
+++ app/controllers/work_subcontractor_details_controller.rb
@@ -34,8 +34,8 @@ class WorkSubcontractorDetailsController < ApplicationController
     # create処理
     if params[:subcontractor_detail][:subcontractor_id].present?
 
-      WorkSubcontractor.find(params[:subcontractor_detail][:subcontractor_id]).detail.create!(count: 1, number_of_copies: 1, deliver_at: DateTime.now, cost_unit_price: 0, estimated_cost: 0, actual_count: 1, actual_cost: 0, work_id: params[:work_id])
-      render json: { status: :success, detail: Work.find(params[:work_id]).subcontractor_detail }
+      detail = WorkSubcontractor.find(params[:subcontractor_detail][:subcontractor_id]).detail.create!(count: 1, number_of_copies: 1, deliver_at: DateTime.now, cost_unit_price: 0, estimated_cost: 0, actual_count: 1, actual_cost: 0, work_id: params[:work_id])
+      render json: { status: :success, detail: detail }
 
     # update処理
     else
