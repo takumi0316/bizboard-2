a003e1fa92cb5b861f759e034d2bbd4c0941678b
takumi <t.okada@jiinet.co.jp>
2019-07-25 18:48:39 +0900

    modify[invoice_search]

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -74,36 +74,24 @@ class Invoice < ApplicationRecord
 
     _self = self
 
-    #そもそも日付選択されてんのか
-    if parameters[:date1] = "InvalidDate"
+    # フリーワードが入っている場合
+    if parameters[:name].present?
 
+      # 名称検索
+      _self = self.date_in(parameters[:date1].to_datetime.beginning_of_day..parameters[:date2].to_datetime.end_of_day)
       terms = parameters[:name].to_s.gsub(/(?:[[:space:]%_])+/, ' ').split(' ')[0..1]
       query = (['free_word like ?'] * terms.size).join(' and ')
       _self = where(query, *terms.map { |term| "%#{term}%" })
-
+      # 日付検索
       return _self
-    else
-      #検索に情報が入力されているか
-      if parameters[:name].present?
+    # フリーワードが空の場合
+    elsif parameters[:name].blank?
 
-        _self = Quote.all.deliverd_in(parameters[:date1].to_datetime.beginning_of_day..parameters[:date2].to_datetime.end_of_day)
-        # 名称検索
-        terms = parameters[:name].to_s.gsub(/(?:[[:space:]%_])+/, ' ').split(' ')[0..1]
-        query = (['free_word like ?'] * terms.size).join(' and ')
-        _self = where(query, *terms.map { |term| "%#{term}%" })
-
-        return _self
-      else
-
-        # 日付検索
-        _self = Quote.all.deliverd_in(parameters[:date1].to_datetime.beginning_of_day..parameters[:date2].to_datetime.end_of_day)
-
-        return _self
-
-      end
+      #日付検索
+      _self = self.date_in(parameters[:date1].to_datetime.beginning_of_day..parameters[:date2].to_datetime.end_of_day)
+      return _self
     end
-
+     return _self
   end
-
 
 end
