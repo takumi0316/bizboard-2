673f3f07484e6658196ab515fef387bde1defa82
takumi <t.okada@jiinet.co.jp>
2019-09-06 16:54:23 +0900

    modify[production.rb]

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,14 +7,14 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server 'factory', user: 'jii', roles: %w{app assets batch db}
+server 'factory', user: 'media', roles: %w{app assets batch db}
 
 set :assets_roles, [:assets]
 
 # rails
 set :rails_env, 'production'
 set :branch, 'master'
-set :deploy_to, '/home/jii/factory'
+set :deploy_to, '/home/media'
 set :log_level, :debug
 set :default_shell, '/bin/bash -l'
 
