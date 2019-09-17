3a25bc812274ab194f89510fd629545f91e40f4b
tymnr <m.toya@jiinet.co.jp>
2019-09-17 17:27:01 +0900

    [fix] 既読処理

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,7 +27,8 @@ class TasksController < ApplicationController
   #----------------------------------------
   #  ** Request cycles **
   #----------------------------------------
-
+  #既読判定
+  before_action :read_message, only: :show
   #----------------------------------------
   #  ** Actions **
   #----------------------------------------
@@ -70,4 +71,13 @@ class TasksController < ApplicationController
   #----------------------------------------
   private
 
+    ##
+    # 既読処理
+    # @version 2018/06/10
+    #
+    def read_message
+      # ログインユーザーを特定しカラムアップデート
+      users = User.find_by(email: current_user[:email])
+      users.update_columns(lastaccesstask: DateTime.now)
+    end
 end
