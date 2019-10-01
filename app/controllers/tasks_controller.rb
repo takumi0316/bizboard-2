26099a1dadc90558621c5f6682597fb3c202ba8c
takumi <t.okada@jiinet.co.jp>
2019-10-01 13:15:25 +0900

    modify[pry外し]

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -57,7 +57,6 @@ class TasksController < ApplicationController
    message.content = params[:task][:messages_attributes][:content] unless params[:task][:messages_attributes][:content].blank?
    message.attached_files = params[:task][:attached_files] unless params[:task][:attached_files].blank?
    message.save!
-   binding.pry
    if task.quote.client.lastaccesstask < message.created_at
      # メール送信
      TaskMailer.read(task.quote.client.id,task.id,message.id).deliver_later if message.created_at > task.clientlastaccess
