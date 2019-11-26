54cb0413a8bec71a6cd39895837b0c8e0c464a88
takumi <t.okada@jiinet.co.jp>
2019-11-26 17:48:28 +0900

    modify[inquiry_model]

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -60,24 +60,7 @@ class Inquiry < ApplicationRecord
   #----------------------------------------
 
   def self.import(file)
-    #ヘッダー情報をHEADER_TO_SYM_MAPに置き換える準備
-    header_converter = lambda { |h| HEADER_TO_SYM_MAP[h] }
-    #csvを読み込む(ヘッダー情報はHEADER_TO_SYM_MAPで指定した内容に置き換わる)
-    csv = CSV.read(file.path, headers: :first_row, header_converters: header_converter, converters: :integer, skip_blanks: true, encoding: "SJIS:UTF-8")
-    #project_number(案件番号)でgroup_by
-    group = csv.group_by{|u| u[:project_number] }
-    #案件番号毎にeach処理
-    group.each do |row,r|
-      quote_id = Quote.all.find_by(quote_number: row).id
-    #csvと同じ案件番号
-    elsif quote_id.blank?
-      #同じ案件番号の内容でeach処理
-      r.each do |ri|
-
-      end
-    else
-
-    end
+    
   end
 
 end
