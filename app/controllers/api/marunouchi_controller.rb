af888348b578beb9bd483292d96a72df675b939b
YutoOshima <oshima1998@gmail.com>
2020-03-10 16:28:40 +0900

    api/marunouchi: exposerを変更

--- app/controllers/api/marunouchi_controller.rb
+++ app/controllers/api/marunouchi_controller.rb
@@ -1,14 +1,14 @@
 class Api::MarunouchiController < ApplicationController
 
-  expose(:all) {
+  expose_with_pagination(:all) {
     Invoice.joins(:quote).where('quotes.division_id': 5).where(date: (params[:sdate].present?? params[:sdate] : Time.current.beginning_of_month)..(params[:edate].present?? params[:edate] : Time.current.beginning_of_month)).order(date: :asc)
   }
 
-  expose(:card) {
+  expose_with_pagination(:only_card) {
     Invoice.joins(:quote).where('quotes.division_id': 5).where('quotes.subject like ?', '%名刺%').where(date: (params[:sdate].present?? params[:sdate] : Time.current.beginning_of_month)..(params[:edate].present?? params[:edate] : Time.current.beginning_of_month)).order(date: :asc)
   }
 
-  expose(:consul) {
+  expose_with_pagination(:consul) {
     Invoice.joins(:quote).where('quotes.division_id': 10).where(date: (params[:sdate].present?? params[:sdate] : Time.current.beginning_of_month)..(params[:edate].present?? params[:edate] : Time.current.beginning_of_month)).order(date: :asc)
   }
 
@@ -19,7 +19,7 @@ class Api::MarunouchiController < ApplicationController
         # 置いておかないとエラーになる
       end
       format.csv do
-        @all = all
+        @all = Invoice.joins(:quote).where('quotes.division_id': 5).where(date: (params[:sdate].present?? params[:sdate] : Time.current.beginning_of_month)..(params[:edate].present?? params[:edate] : Time.current.beginning_of_month)).order(date: :asc)
         send_data render_to_string, filename: "丸の内全案件_#{params[:sdate].to_datetime.month}月.csv", type: :csv
       end
     end
@@ -36,7 +36,7 @@ class Api::MarunouchiController < ApplicationController
         # 置いておかないとエラーになる
       end
       format.csv do
-        @only_card = only_card
+        @only_card = Invoice.joins(:quote).where('quotes.division_id': 5).where('quotes.subject like ?', '%名刺%').where(date: (params[:sdate].present?? params[:sdate] : Time.current.beginning_of_month)..(params[:edate].present?? params[:edate] : Time.current.beginning_of_month)).order(date: :asc)
         send_data render_to_string, filename: "丸の内案件(名刺のみ)_#{params[:sdate].to_datetime.month}月.csv", type: :csv
       end
     end
@@ -53,12 +53,12 @@ class Api::MarunouchiController < ApplicationController
         # 置いておかないとエラーになる
       end
       format.csv do
-        @consul = consul
+        @consul = Invoice.joins(:quote).where('quotes.division_id': 10).where(date: (params[:sdate].present?? params[:sdate] : Time.current.beginning_of_month)..(params[:edate].present?? params[:edate] : Time.current.beginning_of_month)).order(date: :asc)
         send_data render_to_string, filename: "コンサル2Gr全案件_#{params[:sdate].to_datetime.month}月.csv", type: :csv
       end
     end
 
-  rescue => e
+	rescue => e
 
     flash[:warning] = { message: e.message }
   end
