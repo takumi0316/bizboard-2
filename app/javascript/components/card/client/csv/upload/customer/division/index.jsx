2bcd6e74785f4ee70be87103eb07154ab2b96b0a
YutoOshima <oshima1998@gmail.com>
2020-04-14 17:03:20 +0900

    作業ログ;

---
+++ app/javascript/components/card/client/csv/upload/customer/division/index.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+import Infomation from './infomation';
+import Search      from './search';
+
+const Division = props => {
+
+  return(
+    <Fragment>
+      <Infomation company={ props.company } division={ props.division }/>
+      <Search typeName={ props.typeName } notFound={ props.notFound  } applyDivision={ props.applyDivision }/>
+    </Fragment>
+  );
+};
+
+export default Division;
