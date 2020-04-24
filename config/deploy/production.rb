c7ffb87ef78484d275cbae82389f5299b8dfdbb7
YutoOshima <oshima1998@gmail.com>
2020-04-24 18:44:39 +0900

    memory changed

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -33,7 +33,7 @@ namespace :deploy do
   before :compile_assets, :set_max_heep_size do
     on roles(:assets) do
       within release_path do
-        execute "export NODE_OPTIONS='--max-old-space-size=256'"
+        execute "export NODE_OPTIONS='--max-old-space-size=200'"
       end
     end
   end
