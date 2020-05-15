b0229166dc398ef225ecdd7f14a900534c8b53f8
YutoOshima <oshima1998@gmail.com>
2020-05-15 09:18:50 +0900

    メモリ変更;

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -30,7 +30,7 @@ namespace :deploy do
   before :compile_assets, :set_max_heep_size do
     on roles(:assets) do
       within release_path do
-        execute "export NODE_OPTIONS='--max-old-space-size=1100'"
+        execute "export NODE_OPTIONS='--max-old-space-size=1200'"
       end
     end
   end
