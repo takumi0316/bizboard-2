cacf9777e6fc2e352358b906e95451e58636766b
takumi <t.okada@jiinet.co.jp>
2020-06-24 14:51:58 +0900

    modify[グラフ修正]

--- app/controllers/profit_graphs_controller.rb
+++ app/controllers/profit_graphs_controller.rb
@@ -29,6 +29,12 @@ class ProfitGraphsController < ApplicationController
   def index
 
     add_breadcrumb '受注管理'
+    quote_ids = Quote.where(division_id: 2).pluck(:id)
+    activity = Activity.where(quote_id: quote_ids).joins(:quote)
+    hash = activity.group(:quote_id).having('count(*) >= 2').maximum(:created_at)
+    activity_ids = activity.where(quote_id: hash.keys).where.not(created_at: hash.values).pluck(:id)
+    activity_ids = activity.ids if hash.blank?
+
   end
 
 
