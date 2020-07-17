a64379102b2ae929f1a81a2dbbaa5b6a05d111f6
YutoOshima <47956167+yuto-oshima@users.noreply.github.com>
2020-07-17 12:21:30 +0900

    Issues#46 (#184)
    * 一旦commit
    
    * 一旦commit
    
    * fix[一旦commit]
    
    * fix[ステージングで試すよう]
    
    * fix[ステージングで試すよう]
    
    * fix[styleのプロパティ指定が間違っていた？のでステージングおかしかった
    
    * fix[contentに枠線を表示]
    
    * fix[contentに枠線を表示]
    
    * fix[複製案件の受注区分を引き継げるように]

--- app/javascript/components/card/util.js
+++ app/javascript/components/card/util.js
@@ -64,8 +64,12 @@ export const mmTopx = mm => {
  * PDFを展開する
  * @version 2020/03/30
  * 
+ * サイズ: pt
+ * 座標: mm
+ * 長さ: mm
+ * 行間: mm*
  */
-export const setPDF = (file, details, canvas, draw_canvas) => {
+export const setPDF = (file, details, canvas, draw) => {
 
   const blob = new Blob([file]);
   const blob_path = (window.URL || window.webkitURL).createObjectURL(blob);
@@ -83,40 +87,72 @@ export const setPDF = (file, details, canvas, draw_canvas) => {
 
     // Fetch canvas' 2d context
     let ctx = canvas.getContext('2d');
-    let draw_ctx = draw_canvas.getContext('2d');
-
-    // Set dimensions to Canvas
+  
+    /*
+    let draw_ctx = draw.getContext('2d');
     canvas.height = (mmTopx(55 * 2));
     canvas.width = (mmTopx(91 * 2));
-
     draw_canvas.height = (mmTopx(55 * 2));
     draw_canvas.width = (mmTopx(91 * 2));
-
-    details.forEach(detail => {
+ 
+     details.map((detail, index) => {
+ 
+     const name = detail.name;
+ 
+     if(!name) return;
+ 
+     const y = (mmTopx(detail.coord_y)) * 2;
+     const x =	(mmTopx(detail.coord_x)) * 2;
+     const fontSize = (mmTopx(ptTomm(detail.font_size))) * 2;
+     const lineSpace = (mmTopx(detail.line_space));
+     const contentLength = (mmTopx(detail.length));
+ 
+     // 自動組版
+     // draw_ctx.canvas.style.letterSpacing = lineSpace + 'px';
+     // draw_ctx.font = `${ fontSize }px ${ detail.font }`;
+     // draw_ctx.fillText(name, x, y, contentLength, 400);
+     });
+     */
+  
+    // Set dimensions to Canvas
+    canvas.height = Math.floor(mmTopx(55 * 2));
+    canvas.width = Math.floor(mmTopx(91 * 2));
+ 
+    draw.style = `height: ${ Math.floor(mmTopx(55 * 2)) }px; width: ${ Math.floor(mmTopx(91 * 2)) }px;}}`;
+    
+    details.map((detail, index) => {
   
       const name = detail.name;
       
       if(!name) return;
       
-      const y = mmTopx(detail.coord_y) * 2;
-      const x =	mmTopx(detail.coord_x) * 2;
-      const fontSize = mmTopx(ptTomm(detail.font_size)) * 2;
-      const lineSpace = mmTopx(detail.line_space);
+      const y = Math.floor(mmTopx(detail.coord_y)) * 2;
+      const x =	Math.floor(mmTopx(detail.coord_x)) * 2;
+      const fontSize = Math.floor(mmTopx(ptTomm(detail.font_size))) * 2;
+      const lineSpace = Math.floor(mmTopx(detail.line_space));
+      const contentLength = Math.floor(mmTopx(detail.length));
   
-      draw_ctx.font = `${ fontSize }px ${ detail.font }`;
+      // absoluteするための親div
+      let parent_div = document.createElement('div');
+      parent_div.id = `parent_div-${ index }`;
+      // 以下、子ども
+      let child_p = document.createElement('p');
+      child_p.id = `child_p-${ index }`;
+      let child_div = document.createElement('div');
+      child_div.id = `child_div-${ index }`;
+  
+      parent_div.style = `position: relative; transform: translate(${ x }px, ${ y }px);`;
+      draw.appendChild(parent_div);
       
-      for(let lines = name.split('\n'), i = 0, l = lines.length; l > i; i++) {
-        
-        let line = lines[i];
-        let addY = fontSize;
-        if(i) addY += fontSize * lineSpace * i;
-        draw_ctx.fillText(line, x, y + addY);
-      };
-  
-      // 自動組版
-      // draw_ctx.canvas.style.letterSpacing = lineSpace + 'px';
-      // draw_ctx.font = `${ fontSize }px ${ detail.font }`;
-      // draw_ctx.fillText(name, x, y);
+      child_p.textContent = name || '';
+      // transform: translate(x, y)
+      // ヘッダー表示のためword-wrapはなし
+      child_p.style = `font-size: ${ fontSize }px; font-family: ${ detail.font }; letter-spacing: ${ lineSpace }px; position: absolute;`;
+      parent_div.appendChild(child_p);
+      
+      // 先に描画をしないと高さを取得出来ないため
+      child_div.style = `width: ${ contentLength }px; height: ${ child_p.clientHeight }px; border: 1px solid; position: absolute;`;
+      parent_div.appendChild(child_div);
     });
 
     // Prepare object needed by render method
@@ -133,8 +169,13 @@ export const setPDF = (file, details, canvas, draw_canvas) => {
 /**
  * PDFを展開する
  * @version 2020/03/30
+ * 
+ * サイズ: pt
+ * 座標: mm
+ * 長さ: px
+ * 行間: px
  */
-export const setPDFValue = (file, canvas, draw_canvas, values) => {
+export const setPDFValue = (file, canvas, draw, values) => {
 
   const blob = new Blob([file]);
   const blob_path = (window.URL || window.webkitURL).createObjectURL(blob);
@@ -149,41 +190,78 @@ export const setPDFValue = (file, canvas, draw_canvas, values) => {
     let viewport = page.getViewport({ scale: scale });
 
     let ctx = canvas.getContext('2d');
+  
+    /*
     let draw_ctx = draw_canvas.getContext('2d');
-
     canvas.height = (mmTopx(55 * 2));
     canvas.width = (mmTopx(91 * 2));
-
     draw_canvas.height = (mmTopx(55 * 2));
     draw_canvas.width = (mmTopx(91 * 2));
-
-    values.map(value => {
   
+    values.map(value => {
+    
       const card_value = value.value;
-      
+    
       if(!card_value) return;
-      
+    
       const y = mmTopx(value.coord_y) * 2;
       const x =	mmTopx(value.coord_x) * 2;
       const fontSize = mmTopx(ptTomm(value.font_size)) * 2;
       const lineSpace = mmTopx(value.line_space);
-  
+    
       draw_ctx.font = `${ fontSize }px ${ value.font }`;
-      
+    
       for(let lines = card_value.split('\n'), i = 0, l = lines.length; l > i; i++) {
-        
+      
         let line = lines[i] ;
         let addY = fontSize ;
         if (i) addY += fontSize * lineSpace * i ;
         draw_ctx.fillText(line, x, y + addY);
       };
-  
+    
       // 自動組版
       // draw_ctx.canvas.style.letterSpacing = lineSpace + 'px';
       // draw_ctx.font = `${ fontSize }px ${ value.font }`;
       // draw_ctx.fillText(card_value, x, y);
     });
-
+     */
+    
+    canvas.height = Math.floor(mmTopx(55 * 2));
+    canvas.width = Math.floor(mmTopx(91 * 2));
+    draw.style = `height: ${ Math.floor(mmTopx(55 * 2)) }px; width: ${ Math.floor(mmTopx(91 * 2)) }px;`;
+    
+    values.map((value, index) => {
+    
+      const card_value = value.value;
+      const y = Math.floor(mmTopx(value.coord_y) * 2);
+      const x =	Math.floor(mmTopx(value.coord_x) * 2);
+      const fontSize = Math.floor(mmTopx(ptTomm(value.font_size))) * 2;
+      const lineSpace = Math.floor(mmTopx(value.line_space));
+      const contentLength = Math.floor(mmTopx(value.length));
+  
+      // absoluteするための親div
+      let parent_div = document.createElement('div');
+      parent_div.id = `parent_div-${ index }`;
+      // 以下、子ども
+      let child_p = document.createElement('p');
+      child_p.id = `child_p-${ index }`;
+      let child_div = document.createElement('div');
+      child_div.id = `child_div-${ index }`;
+  
+      parent_div.style = `position: relative; transform: translate(${ x }px, ${ y }px);`;
+      draw.appendChild(parent_div);
+  
+      child_p.textContent = card_value || '';
+      // transform: translate(x, y)
+      // ヘッダー表示のためword-wrapはなし
+      child_p.style = `font-size: ${ fontSize }px; font-family: ${ value.font }; letter-spacing: ${ lineSpace }px; word-wrap: break-word; position: absolute;`;
+      parent_div.appendChild(child_p);
+  
+      // 先に描画をしないと高さを取得出来ないため
+      child_div.style = `width: ${ contentLength }px; height: ${ child_p.clientHeight }px; border: 1px solid; position: absolute;`;
+      parent_div.appendChild(child_div);
+    });
+    
     const renderContext = {
       canvasContext: ctx,
       viewport: viewport
@@ -197,51 +275,101 @@ export const setPDFValue = (file, canvas, draw_canvas, values) => {
  * PDFにテキストを展開
  * @version 2020/04/06
  * 
+ * サイズ: pt
+ * 座標: mm
+ * 長さ: px
+ * 行間: px
  */
-export const drawText = (details, draw_canvas) => {
-
-  let draw_ctx = draw_canvas.getContext('2d')
-  draw_ctx.beginPath();
-  draw_ctx.clearRect(0, 0, draw_canvas.width, draw_canvas.height);
-  draw_ctx.save();
-  draw_ctx.setTransform(1, 0, 0, 1, 0, 0);
-  draw_ctx.restore();
-
-  details.map(detail => {
+export const drawText = (detail, draw, index) => {
   
-    const name = detail.name;
-    
-    if(!name) return;
-    
-    const y = mmTopx(detail.coord_y) * 2;
-    const x =	mmTopx(detail.coord_x) * 2;
-    const fontSize = mmTopx(ptTomm(detail.font_size)) * 2;
-    const lineSpace = mmTopx(detail.line_space);
-  
-    draw_ctx.font = `${ fontSize }px ${ detail.font }`;
+  /*
+   let draw_ctx = draw_canvas.getContext('2d')
+   draw_ctx.beginPath();
+   draw_ctx.clearRect(0, 0, draw_canvas.width, draw_canvas.height);
+   draw_ctx.save();
+   draw_ctx.setTransform(1, 0, 0, 1, 0, 0);
+   draw_ctx.restore();
    
-    for(let lines = name.split('\n'), i = 0, l = lines.length; l > i; i++) {
-      
-      let line = lines[i];
-      let addY = fontSize;
-      if(i) addY += fontSize * lineSpace * i;
-      draw_ctx.fillText(line, x, y + addY);
-    };
+   details.map(detail => {
+   
+   const name = detail.name;
+   
+   if(!name) return;
+   
+   const y = (mmTopx(detail.coord_y)) * 2;
+   const x =	(mmTopx(detail.coord_x)) * 2;
+   const fontSize =  (mmTopx(ptTomm(detail.font_size))) * 2;
+   const lineSpace = (mmTopx(detail.line_space));
+   
+   draw_ctx.font = `${ fontSize }px ${ detail.font }`;
+   
+   // 自動組版
+   // draw_ctx.canvas.style.letterSpacing = lineSpace + 'px';
+   // draw_ctx.font = `${ fontSize }px ${ detail.font }`;
+   // draw_ctx.fillText(name, x, y);
+   });
+   */
+
+  const childElement = draw.childElementCount;
+  const name = detail.name;
+  const y = Math.floor(mmTopx(detail.coord_y)) * 2;
+  const x =	Math.floor(mmTopx(detail.coord_x)) * 2;
+  const fontSize = Math.floor(mmTopx(ptTomm(detail.font_size))) * 2;
+  const lineSpace = Math.floor(mmTopx(detail.line_space));
+  const contentLength = Math.floor(mmTopx(detail.length));
+  const font = detail.font;
+  
+  let parent_div, child_div, child_p;
+  // 既に作成済み
+  if(index < childElement) {
     
-    // 自動組版
-    // draw_ctx.canvas.style.letterSpacing = lineSpace + 'px';
-    // draw_ctx.font = `${ fontSize }px ${ detail.font }`;
-    // draw_ctx.fillText(name, x, y);
-  });
+    parent_div = document.getElementById(`parent_div-${ index }`);
+    parent_div.style = `position: relative; transform: translate(${ x }px, ${ y }px);`;
+    
+    child_p = document.getElementById(`child_p-${ index }`);
+    child_p.textContent = name || '';
+    child_p.style = `font-size: ${ fontSize }px; font-family: ${ font }; letter-spacing: ${ lineSpace }px; position: absolute;`;
+    
+    child_div = document.getElementById(`child_div-${ index }`);
+    child_div.style = `width: ${ contentLength }px; height: ${ child_p.clientHeight }px; border: 1px solid; position: absolute;`;
+  } else {
+    
+    // absoluteするための親div
+    parent_div = document.createElement('div');
+    parent_div.id = `parent_div-${ index }`;
+    // 以下、子ども
+    child_p = document.createElement('p');
+    child_p.id = `child_p-${ index }`;
+    child_div = document.createElement('div');
+    child_div.id = `child_div-${ index }`;
+    
+    parent_div.style = `position: relative; transform: translate(${ x }px, ${ y }px);`;
+    draw.appendChild(parent_div);
+  
+    child_p.textContent = name;
+    // transform: translate(x, y)
+    // ヘッダー表示のためword-wrapはなし
+    child_p.style = `font-size: ${ fontSize }px; font-family: ${ font }; letter-spacing: ${ lineSpace }px; position: absolute;`;
+    parent_div.appendChild(child_p);
+  
+    // 先に描画をしないと高さを取得出来ないため
+    child_div.style = `width: ${ contentLength }px; height: ${ child_p.clientHeight }px; border: 1px solid; position: absolute;`;
+    parent_div.appendChild(child_div);
+  };
 };
 
 /**
  * PDFにテキストを展開
  * @version 2020/04/06
  *
+ * サイズ: pt
+ * 座標: mm
+ * 長さ: px
+ * 行間: px
  */
-export const drawTextValue = (values, draw_canvas) => {
+export const drawTextValue = (value, draw, index) => {
 
+  /*
   let draw_ctx = draw_canvas.getContext('2d');
 
   draw_ctx.beginPath();
@@ -249,39 +377,64 @@ export const drawTextValue = (values, draw_canvas) => {
   draw_ctx.save();
   draw_ctx.setTransform(1,0,0,1,0,0);
   draw_ctx.restore();
-
-  values.map(value => {
+ 
+   values.map(value => {
+ 
+   const card_value = value.value;
+ 
+   if(!card_value) return;
+ 
+   const y = mmTopx(value.coord_y) * 2;
+   const x =	mmTopx(value.coord_x) * 2;
+   const fontSize = mmTopx(ptTomm(value.font_size)) * 2;
+   const lineSpace = mmTopx(value.line_space);
+ 
+   draw_ctx.font = `${ fontSize }px ${ value.font }`;
+ 
+   for(let lines = card_value.split('\n'), i = 0, l = lines.length; l > i; i++) {
+ 
+   let line = lines[i] ;
+   let addY = fontSize ;
+   if (i) addY += fontSize * lineSpace * i ;
+   draw_ctx.fillText(line, x, y + addY);
+   };
+ 
+   // 自動組版
+   // draw_ctx.canvas.style.letterSpacing = lineSpace + 'px';
+   // draw_ctx.font = `${ fontSize }px ${ value.font }`;
+   // draw_ctx.fillText(card_value, x, y);
+   });
+   */
   
-    const card_value = value.value;
-    
-    if(!card_value) return;
-    
-    const y = mmTopx(value.coord_y) * 2;
-    const x =	mmTopx(value.coord_x) * 2;
-    const fontSize = mmTopx(ptTomm(value.font_size)) * 2;
-    const lineSpace = mmTopx(value.line_space);
+  const childElement = draw.childElementCount;
+  const card_value = value.value;
+  const y = Math.floor(mmTopx(value.coord_y) * 2);
+  const x =	Math.floor(mmTopx(value.coord_x) * 2);
+  const fontSize = Math.floor(mmTopx(ptTomm(value.font_size))) * 2;
+  const lineSpace = Math.floor(mmTopx(value.line_space));
+  const contentLength = Math.floor(mmTopx(value.length));
+  const font = value.font;
   
-    draw_ctx.font = `${ fontSize }px ${ value.font }`;
-    
-    for(let lines = card_value.split('\n'), i = 0, l = lines.length; l > i; i++) {
-      
-      let line = lines[i] ;
-      let addY = fontSize ;
-      if (i) addY += fontSize * lineSpace * i ;
-      draw_ctx.fillText(line, x, y + addY);
-    };
+  let parent_div, child_div, child_p;
+  parent_div = document.getElementById(`parent_div-${ index }`);
+  parent_div.style = `position: relative; transform: translate(${ x }px, ${ y }px);`;
   
-    // 自動組版
-    // draw_ctx.canvas.style.letterSpacing = lineSpace + 'px';
-    // draw_ctx.font = `${ fontSize }px ${ value.font }`;
-    // draw_ctx.fillText(card_value, x, y);
-  });
+  child_p = document.getElementById(`child_p-${ index }`);
+  child_p.textContent = card_value || '';
+  child_p.style = `width: ${ contentLength }px; font-size: ${ fontSize }px; font-family: ${ font }; letter-spacing: ${ lineSpace }px; position: absolute; word-wrap: break-word;`;
+  
+  child_div = document.getElementById(`child_div-${ index }`);
+  child_div.style = `width: ${ contentLength }px; height: ${ child_p.clientHeight }px; border: 1px solid; position: absolute;`;
 };
 
 /**
  *  ファイルドロップ時
  *  @version 2018/06/10
  * 
+ * サイズ: pt
+ * 座標: mm
+ * 長さ: px
+ * 行間: px 
  */
 export const onDrop = (files, templates, front_file, reverse_file, status) => {
 
