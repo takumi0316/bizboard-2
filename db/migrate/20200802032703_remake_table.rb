0a7035c83eff5d1ea54247ef09ff6afde2e55196
yuto-oshima <oshima1998@gmail.com>
2020-08-12 02:40:59 +0900

    fix[migrationを元に戻す]

--- db/migrate/20200802032703_remake_table.rb
+++ db/migrate/20200802032703_remake_table.rb
@@ -2,87 +2,87 @@ class RemakeTable < ActiveRecord::Migration[5.2]
 
   def up
 
-    ## drop_table :cards, options: 'DEFAULT CHARSET=utf8mb4' do |t|
+    drop_table :cards, options: 'DEFAULT CHARSET=utf8mb4' do |t|
 
-    ##   t.references :company, index: true
-    ##   t.string     :name,    comment: '名刺名称'
-    ##   t.text       :free_word
+      t.references :company, index: true
+      t.string     :name,    comment: '名刺名称'
+      t.text       :free_word
 
-    ##   t.timestamps
-    ## end
+      t.timestamps
+    end
 
-    ## create_table :card_templates, options: 'DEFAULT CHARSET=utf8mb4' do |t|
+    create_table :card_templates, options: 'DEFAULT CHARSET=utf8mb4' do |t|
 
-    ##   t.references :card,   index: true
-    ##   t.integer    :status, limit: 1,   default: 0, comment: 'テンプレートの状態'
+      t.references :card,   index: true
+      t.integer    :status, limit: 1,   default: 0, comment: 'テンプレートの状態'
 
-    ##   t.timestamps
-    ## end
+      t.timestamps
+    end
 
-    ## drop_table :card_templates, options: 'DEFAULT CHARSET=utf8mb4' do |t|
+    drop_table :card_templates, options: 'DEFAULT CHARSET=utf8mb4' do |t|
 
-    ##   t.references :card,   index: true
-    ##   t.integer    :status, limit: 1,   default: 0, comment: 'テンプレートの状態'
+      t.references :card,   index: true
+      t.integer    :status, limit: 1,   default: 0, comment: 'テンプレートの状態'
 
-    ##   t.timestamps
-    ## end
+      t.timestamps
+    end
 
-    ## drop_table :template_details, options: 'DEFAULT CHARSET=utf8mb4' do |t|
+    drop_table :template_details, options: 'DEFAULT CHARSET=utf8mb4' do |t|
 
-    ##   t.references :card_template, index: true
-    ##   t.string     :name,          comment: '名刺カラム名'
-    ##   t.string     :font,          comment: 'フォント'
-    ##   t.string     :font_size,     comment: 'フォントサイズ(pt)'
-    ##   t.string     :font_color,    comment: 'フォントカラー'
-    ##   t.string     :coord_x,       comment: '座標(x)'
-    ##   t.string     :coord_y,       comment: '座標(y)'
-    ##   t.string     :length,        comment: '長さ'
-    ##   t.string     :line_space,    comment: '行間'
+      t.references :card_template, index: true
+      t.string     :name,          comment: '名刺カラム名'
+      t.string     :font,          comment: 'フォント'
+      t.string     :font_size,     comment: 'フォントサイズ(pt)'
+      t.string     :font_color,    comment: 'フォントカラー'
+      t.string     :coord_x,       comment: '座標(x)'
+      t.string     :coord_y,       comment: '座標(y)'
+      t.string     :length,        comment: '長さ'
+      t.string     :line_space,    comment: '行間'
 
-    ##   t.timestamps
-    ## end
+      t.timestamps
+    end
 
-    ## drop_table :card_clients, options: 'DEFAULT CHARSET=utf8mb4' do |t|
+    drop_table :card_clients, options: 'DEFAULT CHARSET=utf8mb4' do |t|
 
-    ##   t.references :card, index: true
-    ##   t.references :company_division, index: true
-    ##   t.references :company_division_client, index: true
-    ##   t.text       :free_word
-    ##   t.integer    :status, limit: 1, default: 0
+      t.references :card, index: true
+      t.references :company_division, index: true
+      t.references :company_division_client, index: true
+      t.text       :free_word
+      t.integer    :status, limit: 1, default: 0
 
-    ##   t.timestamps
-    ## end
+      t.timestamps
+    end
 
-    ## drop_table :client_templates, options: 'DEFAULT CHARSET=utf8mb4' do |t|
+    drop_table :client_templates, options: 'DEFAULT CHARSET=utf8mb4' do |t|
 
-    ##   t.references :card_client,   index: true
-    ##   t.references :card_template, index: true
+      t.references :card_client,   index: true
+      t.references :card_template, index: true
 
-    ##   t.timestamps
-    ## end
+      t.timestamps
+    end
 
-    ## drop_table :client_template_values, options: 'DEFAULT CHARSET=utf8mb4' do |t|
+    drop_table :client_template_values, options: 'DEFAULT CHARSET=utf8mb4' do |t|
 
-    ##   t.references :client_template, index: true
-    ##   t.references :template_detail, index: true
-    ##   t.string     :value,           comment: '入力値'
+      t.references :client_template, index: true
+      t.references :template_detail, index: true
+      t.string     :value,           comment: '入力値'
 
-    ##   t.timestamps
-    ## end
+      t.timestamps
+    end
 
-    ## drop_table :task_card_clients, options: 'DEFAULT CHARSET=utf8mb4' do |t|
+    drop_table :task_card_clients, options: 'DEFAULT CHARSET=utf8mb4' do |t|
 
-    ##   t.references :task, index: true
-    ##   t.references :quote, index: true
-    ##   t.references :card_client, index: true
-    ##   t.string     :shipping_address
-    ##   t.integer    :count
+      t.references :task, index: true
+      t.references :quote, index: true
+      t.references :card_client, index: true
+      t.string     :shipping_address
+      t.integer    :count
 
-    ##   t.timestamps
-    ## end
+      t.timestamps
+    end
 
-    ## add_column :company_division_clients, :default_front_template,   :string,                        comment: 'デフォルトのテンプレ(表)'
-    ## add_column :company_division_clients, :default_reverse_template, :string,                        comment: 'デフォルトのテンプレ(裏)'
+    add_column :company_division_clients, :default_front_template,   :string,                        comment: 'デフォルトのテンプレ(表)'
+    add_column :company_division_clients, :default_reverse_template, :string,                        comment: 'デフォルトのテンプレ(裏)'
     add_column :uploads,                  :status,                   :integer, limit: 1, default: 0, comment: 'ステータス'
 
     create_table :card_templates, options: 'DEFAULT CHARSET=utf8mb4' do |t|
