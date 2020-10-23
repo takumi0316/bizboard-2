18be357adaa0ebe3fe1b000be1e22f497ca57e1d
yuto-oshima <oshima1998@gmail.com>
2020-10-23 11:50:19 +0900

    revert partner

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,54 +2,49 @@
 # Companies Controller
 #
 class CompaniesController < ApplicationController
-
+  
   #----------------------------------------
   #  ** Includes **
   #----------------------------------------
-
+  
   #----------------------------------------
   #  ** Instance variables **
   #----------------------------------------
-
-  # 取引先一覧
+  
   expose_with_pagination(:companies) { Company.search(params[:name]).all.reverse_order }
-
-  # 取引先
+  
   expose(:company) { Company.find_or_initialize_by id: params[:id] || params[:company_id] }
-
-  # 部署一覧
-  expose_with_pagination(:divisions) { CompanyDivision.where(company_id: company.id).search(params[:name]).all.reverse_order }
-
+  
   #----------------------------------------
   #  ** Layouts **
   #----------------------------------------
-
+  
   #----------------------------------------
   #  ** Request cycles **
   #----------------------------------------
-
+  
   #----------------------------------------
   #  ** Actions **
   #----------------------------------------
-
+  
   ##
   # 一覧
   # @version 2018/06/10
   #
   def index
-
+    
     unless request.xhr?
-
-      add_breadcrumb '取引先'
+      
+      add_breadcrumb '取引先一覧'
     end
   end
-
+  
   ##
   # 新規作成
   # @version 2018/06/10
   #
   def new
-
+    
     add_breadcrumb '取引先一覧', path: companies_path
     add_breadcrumb '新規作成'
   end
@@ -59,72 +54,78 @@ class CompaniesController < ApplicationController
   # @version 2018/06/10
   #
   def create
-
+  
     company.update! company_params
-
-    redirect_to company_path(company), flash: { notice: { message: '取引先情報を更新しました' } }
+  
+    redirect_to edit_company_path(company), flash: { notice: { message: '取引先情報を更新しました' } }
   rescue => e
-
+  
     redirect_back fallback_location: url_for({ action: :index }), flash: { notice: { message: e.message } }
   end
 
-  def show
-
-    add_breadcrumb '取引先', path: companies_path
-    add_breadcrumb '取引先・部署情報'
+  ##
+  # 編集
+  # @version 2018/06/10
+  #
+  def edit
+    
+    add_breadcrumb '取引先一覧', path: companies_path
+    add_breadcrumb '編集'
+  rescue => e
+    
+    redirect_back fallback_location: url_for({ action: :index }), flash: { notice: { message: e.message } }
   end
-
+  
   ##
   # 更新処理
   # @version 2018/06/10
   #
   def update
-
-    # 取引先情報更新
+    
     company.update! company_params
-
+    
     redirect_back fallback_location: url_for({ action: :index }), flash: { notice: { message: '取引先情報を更新しました' } }
   rescue => e
-
+    
     redirect_back fallback_location: url_for({ action: :index }), flash: { notice: { message: e.message } }
   end
-
+ 
   ##
   # 削除
   # @version 2018/06/10
   #
   def destroy
-
+    
     raise '対象の会社には部署情報が登録されているため削除できません' if company.divisions.exists?
-
+    
     company.destroy!
   rescue => e
-
+    
     flash[:warning] = { message: e.message }
   ensure
-
+    
     redirect_to action: :index
   end
-
+  
   def import_client
-
+    
     # fileはtmpに自動で一時保存される
     Company.import_client(params[:file], params[:company_id])
     redirect_to companies_path, flash: { notice: { message: '' } }
   end
-
+  
   #----------------------------------------
   #  ** Methods **
   #----------------------------------------
-
+  
   private
-
-    ##
-    # パラメータの取得
-    # @version 2018/06/10
-    #
-    def company_params
-
-      params.require(:company).permit :name, :kana, :note, :approval_status, :online_web_business_card
-    end
+  
+  ##
+  # パラメータの取得
+  # @version 2018/06/10
+  #
+  def company_params
+    
+    params.require(:company).permit :name, :kana, :note, :approval_status, :online_web_business_card
+  end
 end
--- app/controllers/company_divisions_controller.rb
+++ app/controllers/company_divisions_controller.rb
@@ -2,53 +2,50 @@
 # Companies Controller
 #
 class CompanyDivisionsController < ApplicationController
-
+  
   #----------------------------------------
   #  ** Includes **
   #----------------------------------------
-
+  
   #----------------------------------------
   #  ** Instance variables **
   #----------------------------------------
-
+  
   expose_with_pagination(:divisions) { CompanyDivision.search(params[:search]).all.order(company_id: :desc) }
-
-  expose_with_pagination(:clients) { CompanyDivisionClient.where(company_division_id: division.id).search(params[:name]).all.reverse_order }
-
-  expose(:company) { Company.find(params[:company_id]) }
-
+  
   expose(:division) { CompanyDivision.find_or_initialize_by id: params[:id] }
-
+  
   #----------------------------------------
   #  ** Layouts **
   #----------------------------------------
-
+  
   #----------------------------------------
   #  ** Request cycles **
   #----------------------------------------
-
+  
   #----------------------------------------
   #  ** Actions **
   #----------------------------------------
-
+  
   ##
-  #  一覧
+  # 一覧
   # @version 2018/06/10
   #
   def index
-
+    
+    add_breadcrumb '部署一覧'
   end
-
+  
   ##
   # 新規作成
   # @version 2018/06/10
   #
   def new
-
-    add_breadcrumb '取引先・部署', path: company_path(params[:company_id])
-    add_breadcrumb '部署作成'
+    
+    add_breadcrumb '部署一覧', path: company_divisions_path
+    add_breadcrumb '新規作成'
   rescue => e
-
+    
     redirect_back fallback_location: url_for({ action: :index }), flash: { notice: { message: e.message } }
   end
 
@@ -57,67 +54,70 @@ class CompanyDivisionsController < ApplicationController
   # @version 2018/06/10
   #
   def create
-
+  
     division.update! division_params
-
-    redirect_to company_division_path(division), flash: { notice: { message: '部署情報を作成しました。' } }
+  
+    redirect_to edit_company_division_path(division), flash: { notice: { message: '取引先部署情報を更新しました' } }
   rescue => e
-
+  
     redirect_back fallback_location: url_for({ action: :index }), flash: { notice: { message: e.message } }
   end
 
   ##
-  # 詳細
+  # 編集
   # @version 2018/06/10
   #
-  def show
-
-    add_breadcrumb '取引先・部署', path: company_path(division.company_id)
-    add_breadcrumb '部署・担当者情報'
+  def edit
+    
+    add_breadcrumb '部署一覧', path: company_divisions_path
+    add_breadcrumb '編集'
+  rescue => e
+    
+    redirect_back fallback_location: url_for({ action: :index }), flash: { notice: { message: e.message } }
   end
-
+  
   ##
   # 更新処理
   # @version 2018/06/10
   #
   def update
-
+    
     division.update! division_params
-
-    redirect_to company_division_path(division), flash: { notice: { message: '取引先部署情報を更新しました。' } }
+    
+    redirect_back fallback_location: url_for({ action: :index }), flash: { notice: { message: '取引先部署情報を更新しました' } }
   rescue => e
-
+    
     redirect_back fallback_location: url_for({ action: :index }), flash: { notice: { message: e.message } }
   end
-
+ 
   ##
   # 削除
   # @version 2018/06/10
   #
   def destroy
-
+    
     raise '対象の部署にはクライアントが情報が登録されているため削除できません' if division.clients.exists?
-
+    
     division.destroy!
-
+    
     redirect_back fallback_location: url_for({ action: :index }), flash: { notice: { message: '取引先部署情報を削除しました' } }
   rescue => e
-
+    
     redirect_back fallback_location: url_for({ action: :index }), flash: { notice: { message: e.message } }
   end
-
+  
   #----------------------------------------
   #  ** Methods **
   #----------------------------------------
-
+  
   private
-
-    ##
-    # パラメータの取得
-    # @version 2018/06/10
-    #
-    def division_params
-
-      params.require(:company_division).permit :company_id, :name, :kana, :zip, :prefecture_id, :address1, :address2, :note
-    end
+  
+  ##
+  # パラメータの取得
+  # @version 2018/06/10
+  #
+  def division_params
+    
+    params.require(:company_division).permit :company_id, :name, :kana, :zip, :prefecture_id, :address1, :address2, :note
+  end
 end
