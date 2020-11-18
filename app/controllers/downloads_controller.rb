cad9c0faa3dce668edc6c90a5816c2e32e0564c4
takumi <t.okada@jiinet.co.jp>
2020-11-18 14:12:58 +0900

    test

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -3,22 +3,10 @@ class DownloadsController < ApplicationController
   #----------------------------------------
   #  ** Includes **
   #----------------------------------------
-
+    require 'base64'
   #----------------------------------------
   #  ** Instance variables **
   #----------------------------------------
-
-  # 見積もり
-  expose_with_pagination(:quotes) {
-
-    Quote
-    .all
-    .search(name: params[:name], status: params[:status], date1: params[:date1], date2: params[:date2])
-    .order(date: :desc)
-  }
-
-  # 見積もり
-  expose(:quote) { Quote.find_or_initialize_by id: params[:id] }
 
   #----------------------------------------
   #  ** Layouts **
@@ -37,12 +25,18 @@ class DownloadsController < ApplicationController
   # @version 2019/03/12
   #
   def delivery_note
-
-    #ec-factoryからのアクセスの場合は拒否する処理
-    add_breadcrumb 'テスト'
-    #暗号化されたパラメーターを復号する処理(できる？)
-    #ecユーザーID->部署IDと案件のID->部署IDを検証して違う部署なら拒否する
-    #pdfダウンロード
+    #暗号化されたパラメーターを復号する処理(ecからparamsで送ったデータを復号できるか不明)
+    quote_id = Base64.decode64(params[:encoded_quote_id])
+    @quote = Quote.find(quote_id)
+    respond_to do |format|
+      format.html do
+        render  pdf: "納品書_#{@quote.id}", # pdfファイルの名前。これがないとエラーが出ます
+                encoding: 'UTF-8',
+                layout: 'layouts/pdf.html.slim',
+                template: 'downloads/delivery_note.html.slim', # テンプレートファイルの指定。viewsフォルダが読み込まれます。
+                show_as_html: params.key?('debug')
+      end
+    end
   end
 
   #----------------------------------------
