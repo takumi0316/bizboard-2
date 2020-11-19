104df48291d06e3526867165ca2f3fcc29263d2d
takumi <t.okada@jiinet.co.jp>
2020-11-20 03:27:08 +0900

    　ロックされた案件ページ

--- app/javascript/components/quote_editor/item_tables/specifications.jsx
+++ app/javascript/components/quote_editor/item_tables/specifications.jsx
@@ -10,12 +10,12 @@ const Specifications = (props) => {
             const key = `specification-${ props.quote_id ? specification.id : specification.uid }-${ index }` ;
             return (
               <tr { ...{key} }>
-                <td><textarea className='c-form-textarea__work-show-input__textarea2' value={ specification.name } onChange={ e => props.setName(index, e.target.value) } /></td>
-                <td><textarea className='c-form-textarea__work-show-input__textarea2' type='textarea' value={ specification.remarks } onChange={ e => props.setQuoteRemarks(index, e.target.value)} /></td>
-                <td><input className='c-form-text' type='number' step='0.1' value={ specification.unit_price } onChange={ e => props.setUnitPrice(index, e.target.value) } /></td>
-                <td><input className='c-form-text' type='number' step='1' value={ specification.unit } onChange={ e => props.setUnit(index, e.target.value) } /></td>
+                <td><textarea className='c-form-textarea__work-show-input__textarea2' value={ specification.name } disabled={props.lock ? 'disabled' : null} onChange={ e => props.setName(index, e.target.value) } /></td>
+                <td><textarea className='c-form-textarea__work-show-input__textarea2' type='textarea' value={ specification.remarks } disabled={props.lock ? 'disabled' : null} onChange={ e => props.setQuoteRemarks(index, e.target.value)} /></td>
+                <td><input className='c-form-text' type='number' step='0.1' value={ specification.unit_price } disabled={props.lock ? 'disabled' : null} onChange={ e => props.setUnitPrice(index, e.target.value) } /></td>
+                <td><input className='c-form-text' type='number' step='1' value={ specification.unit } disabled={props.lock ? 'disabled' : null} onChange={ e => props.setUnit(index, e.target.value) } /></td>
                 <td><input readOnly className='c-form-text' type='text' value={ specification.price }/></td>
-                <td><button className='c-btnMain2-primaryA' value={ index } onClick={ e => props.projectDestroy(e) }>ー</button></td>
+                <td><button className='c-btnMain2-primaryA' value={ index } disabled={props.lock ? 'disabled' : null} onClick={ e => props.projectDestroy(e) }>ー</button></td>
               </tr>
             );
           }) }
