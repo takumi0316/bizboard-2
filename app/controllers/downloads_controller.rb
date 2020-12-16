067a514a179f967f2497b7bd0d7b8bae5af6f7c8
takumi <t.okada@jiinet.co.jp>
2020-12-16 19:15:15 +0900

    modify[修正]

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -28,11 +28,6 @@ class DownloadsController < ApplicationController
     #暗号化されたパラメーターを復号する処理(ecからparamsで送ったデータを復号できるか不明)
     quote_id = Base64.decode64(params[:encoded_quote_id])
     @quote = Quote.find(quote_id)
-    #ここからdelivery_note_controllerに飛ばして処理をしたい。
-    #ecからのアクセスの為ログインしなくてもサイトを覗けるようにしているが、
-    #delivery_note_controllerもログインしなくても覗けるようにするのとURLの案件案件番号を暗号化する処理を別途追加する必要がある。
-    #上記を行なうより同じviewではあるがログインをしないでも覗けるのとURLを暗号化できているdownloadsコントローラーで全て処理した方がいいと思う。
-  
     respond_to do |format|
       format.html do
         render  pdf: "納品書_#{@quote.id}", # pdfファイルの名前。これがないとエラーが出ます
