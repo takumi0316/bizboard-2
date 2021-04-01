2eafee850974f0ed11b4021d2e1b433b17eb190e
yuto-oshima <oshima1998@gmail.com>
2021-04-01 15:56:42 +0900

    [modify]

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -287,7 +287,7 @@ class QuotesController < ApplicationController
                       values << r.head_layout.contents.where(content_flag_id: flag_id).first.content_uploads.first.upload.name
                     else
  
-                      values << layout_value.upload_id.nil? ? '' : layout_value.upload.name
+                      values << layout_value.upload.name || ''
                     end
                   end
 
@@ -306,8 +306,8 @@ class QuotesController < ApplicationController
 
                       values << r.head_layout.contents.where(content_flag_id: flag_id).first.content_uploads.first.upload.name
                     else
-
-                      values << layout_value.upload_id ? layout_value.upload.name : ''
+  
+                      values << layout_value.upload.name || ''
                     end
                   end
 
