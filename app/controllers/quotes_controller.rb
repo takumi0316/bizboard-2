f2eada02bc10a3e1202c68a9c34f646c834c23c9
yuto-oshima <oshima1998@gmail.com>
2021-06-11 16:54:51 +0900

    [modify] output csv 文字化け

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -231,7 +231,7 @@ class QuotesController < ApplicationController
 
     Zip::File.open(fullpath, Zip::File::CREATE) do |zipfile|
       card_template = CardTemplate.find_by(company_id: quote.client.company_division.company_id)
-      zipfile.get_output_stream("#{quote.quote_number}_#{quote.subject}.csv") do |f|
+      zipfile.get_output_stream("#{quote.quote_number}_#{quote.subject}.csv".encode('utf-8')) do |f|
 
         bom = "\uFEFF"
         f.puts(
